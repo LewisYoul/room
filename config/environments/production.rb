67664ff2eaf4584c0f7fdabde534114b54f9845c
LewisYoul <lewisyoul@gmail.com>
2020-03-22 17:29:53 +0000

    [config] Set up redis ENV variables

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,6 +14,8 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+  config.action_cable.allowed_request_origins = ['https://simple-rooms.herokuapp.com', 'http://simple-rooms.herokuapp.com']
+  config.web_socket_server_url = "wss://simple-rooms.herokuapp.com/cable" 
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
   # config.require_master_key = true
