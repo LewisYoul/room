f461eb6520764bc34880e5f41fd39f9c6fd09b4a
LewisYoul <lewisyoul@gmail.com>
2020-03-22 17:36:46 +0000

    [temp] console log

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -21,6 +21,7 @@ export default {
   },
 
   created() {
+    console.log('fds')
     const self = this;
     this.chatChannel = App.cable.subscriptions.create(
       { channel: 'ApplicationCable::ChatChannel', room: 'home' }, {
