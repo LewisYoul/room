613ac5891fb7f4001a9a0a356c9ca58a14966b8a
LewisYoul <lewisyoul@gmail.com>
2020-03-22 18:35:59 +0000

    [feature] Add name input

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,12 +1,17 @@
 <template>
-  <div>
-      <label>Name</label>
-      <input @keyup="updateName($event)">
-      <label>Message</label>
-      <input @keyup="sendMessage($event)">
-      <div v-for="message in messages" :key="message.body">
-        <p><strong>{{ message.sent_by }}: </strong>{{ message.body }}</p>
+  <div style="height: 100%;">
+    <div v-if="!name" id="name-input">
+      <h1>ROOM</h1>
+      <input @keyup="updateName($event)" placeholder="Enter your name">
+    </div>
+    <div class="chat-wrapper" v-else>
+      <div class="conversation-wrapper">
+        <div v-for="message in messages" :key="message.body">
+          <p><strong>{{ message.sent_by }}: </strong>{{ message.body }}</p>
+        </div>
       </div>
+        <textarea class="input" @keyup="sendMessage($event)" placeholder="Type something..."></textarea>
+    </div>
   </div>
 </template>
 
@@ -15,10 +20,11 @@ export default {
   data: function () {
     return {
       chatChannel: null,
-      userName: null,
+      name: null,
       messages: []
     }
   },
+
 
   created() {
     const self = this;
@@ -33,15 +39,19 @@ export default {
 
   methods: {
     updateName(event) {
-      this.userName = event.target.value
+      const name = event.target.value
+
+      if (event.keyCode == 13 && name.length > 0) {
+        this.name = event.target.value
+      }
     },
 
     sendMessage(event) {
       if (event.keyCode == 13) {
-        if (this.userName) {
+        if (this.name) {
           this.chatChannel.send({
             room: "home",
-            sent_by: this.userName,
+            sent_by: this.name,
             body: event.target.value
           })
           event.target.value = '';
@@ -55,8 +65,36 @@ export default {
 </script>
 
 <style scoped>
-/* p {
-  font-size: 2em;
+#name-input {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  margin-top: -50px;
+  margin-left: -100px;
+  width: 200px;
   text-align: center;
-} */
+}
+
+.chat-wrapper {
+  width: 30%;
+  height: 60%;
+  display: flex;
+  flex-direction: column;
+  margin: 10px;
+}
+
+.conversation-wrapper {
+  padding: 6px;
+  flex: 4;
+  border: 2px solid grey;
+  border-radius: 10px;
+}
+
+.input {
+  padding: 6px;
+  flex: 1;
+  border: 2px solid grey;
+  border-radius: 10px;
+  margin-top: 10px;
+}
 </style>
